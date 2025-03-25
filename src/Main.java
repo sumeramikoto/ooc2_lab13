14f1a475e5c48d5749fdfa1b9451af66c10e8f54
Navid Ibrahim <nibrahim7787@gmail.com>
2025-03-26 01:57:10 +0600

    Extracted methods to display admin & user manuals

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@
  *
  */
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -332,48 +330,55 @@ public class Main {
             choice = read.nextInt();
         }
         if (choice == 1) {
-            System.out.println(
-                    "\n\n(1) Admin have the access to all users data...Admin can delete, update, add and can perform search for any customer...\n");
-            System.out.println(
-                    "(2) In order to access the admin module, you've to get yourself register by pressing 2, when the main menu gets displayed...\n");
-            System.out.println(
-                    "(3) Provide the required details i.e., name, email, id...Once you've registered yourself, press 1 to login as an admin... \n");
-            System.out.println(
-                    "(4) Once you've logged in, 2nd layer menu will be displayed on the screen...From here on, you can select from variety of options...\n");
-            System.out.println(
-                    "(5) Pressing \"1\" will add a new Passenger, provide the program with required details to add the passenger...\n");
-            System.out.println(
-                    "(6) Pressing \"2\" will search for any passenger, given the admin(you) provides the ID from the table printing above....  \n");
-            System.out.println(
-                    "(7) Pressing \"3\" will let you update any passengers data given the user ID provided to program...\n");
-            System.out.println("(8) Pressing \"4\" will let you delete any passenger given its ID provided...\n");
-            System.out.println("(9) Pressing \"5\" will let you display all registered passenger...\n");
-            System.out.println(
-                    "(10) Pressing \"6\" will let you display all registered passengers...After selecting, program will ask, if you want to display passengers for all flights(Y/y) or a specific flight(N/n)\n");
-            System.out.println(
-                    "(11) Pressing \"7\" will let you delete any flight given its flight number provided...\n");
-            System.out.println(
-                    "(11) Pressing \"0\" will make you logged out of the program...You can login again any time you want during the program execution....\n");
+            displayAdminManual();
         } else {
-            System.out.println(
-                    "\n\n(1) Local user has the access to its data only...He/She won't be able to change/update other users data...\n");
-            System.out.println(
-                    "(2) In order to access local users benefits, you've to get yourself register by pressing 4, when the main menu gets displayed...\n");
-            System.out.println(
-                    "(3) Provide the details asked by the program to add you to the users list...Once you've registered yourself, press \"3\" to login as a passenger...\n");
-            System.out.println(
-                    "(4) Once you've logged in, 3rd layer menu will be displayed...From here on, you embarked on the journey to fly with us...\n");
-            System.out.println(
-                    "(5) Pressing \"1\" will display available/scheduled list of flights...To get yourself booked for a flight, enter the flight number and number of tickets for the flight...Max num of tickets at a time is 10 ...\n");
-            System.out.println(
-                    "(7) Pressing \"2\" will let you update your own data...You won't be able to update other's data... \n");
-            System.out.println("(8) Pressing \"3\" will delete your account... \n");
-            System.out
-                    .println("(9) Pressing \"4\" will display randomly designed flight schedule for this runtime...\n");
-            System.out.println("(10) Pressing \"5\" will let you cancel any flight registered by you...\n");
-            System.out.println("(11) Pressing \"6\" will display all flights registered by you...\n");
-            System.out.println(
-                    "(12) Pressing \"0\" will make you logout of the program...You can login back at anytime with your credentials...for this particular run-time... \n");
+            displayUserManual();
         }
+    }
+
+    private static void displayAdminManual() {
+        System.out.println(
+                "\n\n(1) Admin have the access to all users data...Admin can delete, update, add and can perform search for any customer...\n");
+        System.out.println(
+                "(2) In order to access the admin module, you've to get yourself register by pressing 2, when the main menu gets displayed...\n");
+        System.out.println(
+                "(3) Provide the required details i.e., name, email, id...Once you've registered yourself, press 1 to login as an admin... \n");
+        System.out.println(
+                "(4) Once you've logged in, 2nd layer menu will be displayed on the screen...From here on, you can select from variety of options...\n");
+        System.out.println(
+                "(5) Pressing \"1\" will add a new Passenger, provide the program with required details to add the passenger...\n");
+        System.out.println(
+                "(6) Pressing \"2\" will search for any passenger, given the admin(you) provides the ID from the table printing above....  \n");
+        System.out.println(
+                "(7) Pressing \"3\" will let you update any passengers data given the user ID provided to program...\n");
+        System.out.println("(8) Pressing \"4\" will let you delete any passenger given its ID provided...\n");
+        System.out.println("(9) Pressing \"5\" will let you display all registered passenger...\n");
+        System.out.println(
+                "(10) Pressing \"6\" will let you display all registered passengers...After selecting, program will ask, if you want to display passengers for all flights(Y/y) or a specific flight(N/n)\n");
+        System.out.println(
+                "(11) Pressing \"7\" will let you delete any flight given its flight number provided...\n");
+        System.out.println(
+                "(11) Pressing \"0\" will make you logged out of the program...You can login again any time you want during the program execution....\n");
+    }
+
+    private static void displayUserManual() {
+        System.out.println(
+                "\n\n(1) Local user has the access to its data only...He/She won't be able to change/update other users data...\n");
+        System.out.println(
+                "(2) In order to access local users benefits, you've to get yourself register by pressing 4, when the main menu gets displayed...\n");
+        System.out.println(
+                "(3) Provide the details asked by the program to add you to the users list...Once you've registered yourself, press \"3\" to login as a passenger...\n");
+        System.out.println(
+                "(4) Once you've logged in, 3rd layer menu will be displayed...From here on, you embarked on the journey to fly with us...\n");
+        System.out.println(
+                "(5) Pressing \"1\" will display available/scheduled list of flights...To get yourself booked for a flight, enter the flight number and number of tickets for the flight...Max num of tickets at a time is 10 ...\n");
+        System.out.println(
+                "(7) Pressing \"2\" will let you update your own data...You won't be able to update other's data... \n");
+        System.out.println("(8) Pressing \"3\" will delete your account... \n");
+        System.out.println("(9) Pressing \"4\" will display randomly designed flight schedule for this runtime...\n");
+        System.out.println("(10) Pressing \"5\" will let you cancel any flight registered by you...\n");
+        System.out.println("(11) Pressing \"6\" will display all flights registered by you...\n");
+        System.out.println(
+                "(12) Pressing \"0\" will make you logout of the program...You can login back at anytime with your credentials...for this particular run-time... \n");
     }
 }
